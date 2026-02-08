3322fb1fd52b1f32e64e626d1923e9280616b638
Himeros04 <himeros.conseil@gmail.com>
2026-02-08 15:12:30 +0100

    Security fix : exposed API KEY

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,13 +18,13 @@ import { getStorage } from 'firebase/storage';
 
 // TODO: Replace with your Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDWpg-2OKnTQBrmjoMo5XsyEY_b6TK6hC8",
-    authDomain: "learningon-guitar.firebaseapp.com",
-    projectId: "learningon-guitar",
-    storageBucket: "learningon-guitar.firebasestorage.app",
-    messagingSenderId: "453234638819",
-    appId: "1:453234638819:web:1dfe57a38f048c85e9aad6",
-    measurementId: "G-YGD92SNSF3"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
